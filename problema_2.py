c1acd58074fde0fd11ef6f0e7b2663c1c4e12cb6
JGalvisS <mejorquemada@hotmail.com>
2026-04-24 18:26:14 -0500

    funcional para el resto del año

--- problema_2.py
+++ problema_2.py
@@ -6,16 +6,17 @@ Codigo fuente: UNAD, adaptacion del estudiante.
 """
 VENTAS_POR_MES = {"enero": 1500, "febrero": 2200, "marzo": 1800}
 LIMITE_BONO = 5000
-#Solicita nombre y ventas del mes de abril para almacenamiento.
+#Solicita nombre, mes actual y una cantidad vendida para almacenamiento.
 def solicitar_datos():
-    """Solicita un nombre y una cantidad al usuario."""
+    """Solicita un nombre, mes actual y una cantidad vendida."""
     nombre_vendedor = input("Ingrese su nombre: ")
     try:
-        cantidad_nueva = int(input("Ingrese las ventas de abril: "))
+        mes_actual=input("Ingrese el mes actulal: ")
+        cantidad_nueva = int(input(f"Ingrese las ventas de {mes_actual}: "))
     except:
         print("Entrada inválida, usando 0.")
         cantidad_nueva = 0
-    return nombre_vendedor, cantidad_nueva
+    return nombre_vendedor, cantidad_nueva, mes_actual
 #Agrega la venta al diccionario de ventas y lo devuelve actualiazado
 def agregar_ventas(datos_actuales, mes, monto):
     """Agrega un nuevo mes de ventas al diccionario."""
@@ -24,20 +25,22 @@ def agregar_ventas(datos_actuales, mes, monto):
 #Revisa si las ventas totales son suficientes para el bono y calcula su valor si es así.
 def revisar_bono(ventas_totales, limite, nuevas_ventas):
     """Verifica si el vendedor califica para un bono."""
-    numero_meses = 4  # enero, febrero, marzo, abril
+    numero_meses = len(VENTAS_POR_MES_NUEVO)
     promedio_ventas = ventas_totales / numero_meses
     if promedio_ventas >= limite:
         monto_bono = int(nuevas_ventas * 0.30)
-        print(f"¡Felicidades! Ganaste un bono de: {monto_bono}")
+        print(f"""________________________________________________________________
+        ¡Felicidades! Ganaste un bono de: {monto_bono}
+________________________________________________________________""")
     else:
         print("Siga esforzándose para el bono.")
 #Imprime resumen por pantalla.        
 while True:
     try:
-        vendedor, nuevas_ventas = solicitar_datos()
-        VENTAS_POR_MES_NUEVO = agregar_ventas(VENTAS_POR_MES, "abril", nuevas_ventas)
+        vendedor, nuevas_ventas, mes_actual = solicitar_datos()
+        VENTAS_POR_MES_NUEVO = agregar_ventas(VENTAS_POR_MES, mes_actual, nuevas_ventas)
         total_anual = sum(VENTAS_POR_MES_NUEVO)
-        revisar_bono(total_anual, LIMITE_BONO, nuevas_ventas)#linea corregida: nombre de variable LIMITE_BONO_INCORRECTO
+        revisar_bono(total_anual, LIMITE_BONO, nuevas_ventas) #linea corregida: nombre de variable LIMITE_BONO_INCORRECTO
         print(f"RESUMEN DE VENTAS:\nVendedor: {vendedor}")
         for mes, ventas in VENTAS_POR_MES.items():
             print(f"Ventas de {mes}: {ventas}")
